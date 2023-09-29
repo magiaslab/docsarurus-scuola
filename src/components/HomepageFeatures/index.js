781719cbea589dc09e01daed4dc1a4e592364394
Alessandro Cipriani <social@magiaslab.com>
2023-09-29 12:53:17 +0200

    page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,11 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Guide per tutti',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Un luogo dove trovare le guide per le componenti digitali della scuola
       </>
     ),
   },
