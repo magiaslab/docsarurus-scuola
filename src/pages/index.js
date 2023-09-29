15adc471c664cf58127a263c2c63e91596cb471a
Alessandro Cipriani <social@magiaslab.com>
2023-09-29 17:02:15 +0200

    emblema

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import DocusaurusImageUrl from '@site/static/img/a-emblema.jpg';
+import DocusaurusImageUrl from '@site/static/img/Emblem_of_Italy.svg';
 
 
 
