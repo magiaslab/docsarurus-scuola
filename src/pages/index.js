3b863c09aabed2914d980fbfda6b6191b798c663
Alessandro Cipriani <social@magiaslab.com>
2023-10-03 08:31:32 +0200

    logorep

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import DocusaurusImageUrl from '@site/static/img/logorepubblica.png';
+import DocusaurusImageUrl from '@site/static/img/logorepok.png';
 
 
 
